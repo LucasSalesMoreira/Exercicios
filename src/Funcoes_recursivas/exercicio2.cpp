f1ad8670bc06af51639abd69b3bb75c7b3e21d7a
LucasSalesMoreira <lucasdevsoftware@gmail.com>
2021-03-01 22:36:30 -0300

    Update

--- src/Funcoes_recursivas/exercicio2.cpp
+++ src/Funcoes_recursivas/exercicio2.cpp
@@ -4,18 +4,15 @@ using namespace std;
 
 double somarIndexs(double array[], int length) {
     if (length == 0)
-        return array[0];
-    else {    
-        return array[length] + somarIndexs(array, length - 1);
-    }
+        return 0;
+    else   
+        return array[length - 1] + somarIndexs(array, length - 1);
 }
 
 int main() {
-    double n[] = { 5, 10.5, 20, 15, 27.5,
-                   5, 10.5, 20, 15, 27.5, 
-                   5, 10.5, 20, 15, 27.5, 
-                   0.5 }; 
+    double n[] = { 5, 10.5, 20, 4 };  
     int length = sizeof(n) / 8;
+    cout << length << endl;
     cout << somarIndexs(n, length);
     return 0;
 }
